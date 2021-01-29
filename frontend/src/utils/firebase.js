e45e03e3989e2a7b672677d2c4820d7b0004c154
Shubham Rawat <rawatrajput7@gmail.com>
2021-01-29 11:41:32 +0530

    Added image support

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,15 +1,8 @@
 import firebase from 'firebase';
 
+// Create a file 'firebasedata.js' and export firebase cofig data in it
+import firebaseConfig from './firebasedata';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: 'AIzaSyATdaLtpc0pAoVFjM6OF_8k-ZD1gdH6W9I',
-    authDomain: 'surface-image.firebaseapp.com',
-    projectId: 'surface-image',
-    storageBucket: 'surface-image.appspot.com',
-    messagingSenderId: '91127725679',
-    appId: '1:91127725679:web:bc16239e1d8184436bd79a',
-    measurementId: 'G-FG3M5KJ9SC'
-};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
